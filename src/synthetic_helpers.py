0a1b0a4cedb1b3fba8fdc3121de04aa3dfdeb870
DimSum2k <dimitri.meunier.21@ucl.ac.uk>
2022-07-12 15:50:56 +0200

    update gmm

--- src/synthetic_helpers.py
+++ src/synthetic_helpers.py
@@ -5,8 +5,8 @@ from torch.distributions.mixture_same_family import MixtureSameFamily
 from torch.distributions.uniform import Uniform
 import torch.nn.functional as F
 
-from classifiers import KRR
-from training import train_multiple_kernels
+from src.classifiers import KRR
+from src.training import train_multiple_kernels
 from SWkernels import k_sw_rf
 
 def GMM(means, covs, p):

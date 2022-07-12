bd66ece6e968638d834bedeb8a8eb5b8790e530c
DimSum2k <dimitri.meunier.21@ucl.ac.uk>
2022-07-12 15:40:56 +0200

    update gmm

--- src/synthetic_helpers.py
+++ src/synthetic_helpers.py
@@ -22,3 +22,40 @@ def GMM(means, covs, p):
     comp = MultivariateNormal(means, covs)
     gmm = MixtureSameFamily(mix, comp)
     return gmm
+
+def sample_moments(T, d, K_max):
+    """Sample moments of T GMMs in dimension d.
+    Args:
+        T (int): number of tasks
+        d (int): input dimension
+        K_max (int): maximum number of components
+    Returns:
+        y (T,), means (list), covs (list)
+    """
+    means_l = []
+    covs_l = []
+    y = Categorical(torch.ones(K_max)).sample((T,)) # uniform draw of modes
+    for t in range(T):
+        K = y[t]+1
+        means = Uniform(low=-5., high=5.).sample((K,d))
+        covs = Uniform(low=-1., high=1.).sample((K, d, d))
+        a = Uniform(low=1., high=4.).sample((K,))
+        B = Uniform(low=0., high=1.).sample((K, d))
+        covs = a.view(-1,1,1) * covs @ torch.transpose(covs, 1, 2) + torch.stack([torch.diag(b) for b in B])
+        means_l.append(means)
+        covs_l.append(covs)
+    y = F.one_hot(y.to(torch.int64))
+    
+    return means_l, covs_l, y
+
+def sample_from_moments(means, covs, n, K_max, epoch):
+    T = len(means)
+    d = means[0].shape[-1]
+    X = torch.zeros((T, epoch, n, d))
+    for t in range(T):
+        K = means[t].shape[0]
+        for e in range(epoch):
+            X[t,e] = GMM(means[t], covs[t], torch.ones(K)).sample((n,))
+    return X
+
+
